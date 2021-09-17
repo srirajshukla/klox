c7dc10dc597aee62de3a0ce39a1ec3b218827e35
Gyanendra Shukla <srirajshukla@gmail.com>
2021-09-17 23:57:40 +0530

    Basic visitor pattern
    The syntax generation for following has been added:
    1. The abstract class for pattern
    2. visitor function implementation in all type classes

--- tool/src/GenerateAst.kt
+++ tool/src/GenerateAst.kt
@@ -22,7 +22,7 @@ class GenerateAst {
             defineAst(outputDir, "Expr", listOf(
                 "Binary   - left: Expr, operator: Token, right: Expr",
                 "Grouping - expressions: Expr",
-                "Literal  - Value: Any",
+                "Literal  - value: Any",
                 "Unary    - operator: Token, right: Expr"
             )
             )
@@ -36,9 +36,12 @@ class GenerateAst {
             writer.println("// ABSTRACT CLASS FOR $baseName")
             writer.println()
             writer.println("abstract class $baseName {")
-            writer.println("  companion object {")
+
+            defineVisitor(writer, baseName, types)
 
             // the ast classes
+            writer.println("  companion object {")
+
             for (type in types){
                 val className = type.split('-')[0].trim()
                 val fields = type.split('-')[1].trim()
@@ -46,10 +49,26 @@ class GenerateAst {
                 defineType(writer, baseName, className, fields)
             }
 
+            // closing of the companion object
             writer.println("  }")
 
+            writer.println()
+            writer.println("abstract fun <R> accept(visitor: Visitor<R>) : R")
+
+            // closing of the base class
             writer.println("}")
             writer.close()
+        }
+
+        private fun defineVisitor(writer: PrintWriter, baseName: String, types: List<String>) {
+            writer.println("interface Visitor<R> {")
+
+            for (type in types){
+                val typeName = type.split("-")[0].trim()
+                writer.println("fun visit$typeName$baseName (${baseName.lowercase()}: $typeName) : R")
+            }
+
+            writer.println("}")
         }
 
         private fun defineType(writer: PrintWriter, baseName: String, className: String, fieldsList: String) {
@@ -67,6 +86,12 @@ class GenerateAst {
             }
             writer.println("  }")
 
+            writer.println()
+            writer.println("   override fun<R> accept(visitor: Visitor<R>) : R {")
+            writer.println("      return visitor.visit$className$baseName(this)")
+            writer.println("    }")
+
+            // closing of the type class
             writer.println("}")
         }
     }
