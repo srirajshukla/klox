8e35b682de28da43e846143d24edcf6890b88e99
Gyanendra Shukla <srirajshukla@gmail.com>
2021-11-26 22:37:46 +0530

    feat: add enclosing environments
    1. add scoping support in environments

--- src/main/kotlin/Environment.kt
+++ src/main/kotlin/Environment.kt
@@ -1,6 +1,14 @@
 class Environment {
     private var values = hashMapOf<String, Any?>()
+    private var enclosing : Environment? = null
 
+    constructor() {
+        this.enclosing = null
+    }
+
+    constructor(enclosing: Environment) {
+        this.enclosing = enclosing
+    }
 
     fun define(name: String, value: Any?) {
         values.put(name, value)
@@ -9,6 +17,10 @@ class Environment {
     fun get(name: Token) : Any? {
         if (values.containsKey(name.lexeme))
             return values.get(name.lexeme)
+
+        enclosing?.get(name)    // If enclosing is not null then you can try
+                                // getting value from the enclosing (parent) scope
+
         throw RuntimeError(name, "Undefined Variable ${name.lexeme}.")
     }
 
@@ -17,6 +29,9 @@ class Environment {
             values.put(name.lexeme, value)
             return
         }
+
+        enclosing?.assign(name, value)  // if the variable is not in this scope
+                                        // try assigning it in the parent scope
 
         throw RuntimeError(name, "Undefined Variable ${name.lexeme}.")
     }
